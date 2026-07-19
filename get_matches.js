6df2f9f7069e449bf8397497cea51b0ef019db76
Shaun Abbe <shaunamugabe@gmail.com>
2026-07-19 18:26:38 +0200

    Major 2

--- get_matches.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('c:/Users/user/Documents/antrizy/index.html', 'utf-8');
-const wixMatches = content.match(/.{0,20}wixstatic.{0,20}/g) || [];
-const paraMatches = content.match(/.{0,20}parastorage.{0,20}/g) || [];
-fs.writeFileSync('c:/Users/user/Documents/antrizy/matches.txt', wixMatches.slice(0,20).join('\n') + '\n---\n' + paraMatches.slice(0,20).join('\n'));
