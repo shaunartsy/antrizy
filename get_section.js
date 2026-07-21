ae692a47c158ed7b385cf391a480783f85ac78df
Shaun Abbe <shaunamugabe@gmail.com>
2026-07-21 15:17:06 +0200

    Major2

--- get_section.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let h = fs.readFileSync('index.html', 'utf8');
-let start = h.indexOf('<section id="comp-lpj1bhfh"');
-let end = h.indexOf('</section>', start) + 10;
-console.log(h.substring(start, end));
